97c74ca38797ec7a0549c727033510d8881f4570
Roya <royash-bb104@code.edu.az>
2025-05-04 17:22:50 -0700

    second

---
+++ Techan/Models/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Techan.Models
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Contact() 
+        {
+            return View();
+        }
+    }
+}
